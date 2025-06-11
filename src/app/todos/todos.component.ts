578115445ca4fe8cfd4c91616dd21c94ea3eb2be
John.md <106608808+JohnVLN@users.noreply.github.com>
2025-06-11 15:42:54 -0300

    Rendering info from a service.

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
+import { Todo } from '../components/model/todo.type';
 
 @Component({
   selector: 'app-todos',
@@ -10,8 +11,10 @@ import { TodosService } from '../services/todos.service';
 })
 export class TodosComponent implements OnInit {
   todoservice = inject(TodosService); // injects the todo service in component
+  todoItems = signal<Array<Todo>>([]) // sets the information took from service as signal
 
   ngOnInit(): void {
     console.log(this.todoservice.todoItems); // logs the items in console
+    this.todoItems.set(this.todoservice.todoItems); // takes information from service
   }
 }
